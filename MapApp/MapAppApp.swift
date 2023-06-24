4e0efd75af03250dd6b799426af3edf32941057e
Chetan Choudhary <98541541+AlfieSolomons09@users.noreply.github.com>
2023-06-24 17:41:02 +0530

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Chetan Choudhary on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
