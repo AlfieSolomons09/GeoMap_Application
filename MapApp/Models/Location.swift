0460b7bb7f221b1bc9bd9fb17fbfdf3ce5a0979d
Chetan Choudhary <98541541+AlfieSolomons09@users.noreply.github.com>
2023-06-27 21:41:59 +0530

    Project Completed

--- MapApp/Models/Location.swift
+++ MapApp/Models/Location.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import MapKit
+
+struct Location: Identifiable, Equatable{
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    // Identifiable
+    var id: String{
+        // name =  "Collosseum
+        // cityName = "Rome"
+        // id = "CollosseumRome"
+        name + cityName
+    }
+    
+    // Equatable
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+}
--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -6,3 +6,80 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
+
+class LocationsViewModel: ObservableObject{
+    
+    // All loaded locations
+    @Published var locations: [Location]
+    
+    // Current location on the map
+    @Published var mapLocation: Location{
+        didSet{
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    // Current Region On Map
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    // Show list of locations
+    @Published var showLocationsList: Bool = false
+    
+    // Show location Detail via sheet
+    @Published var sheetLocation: Location? = nil
+    
+    init(){
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location){
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
+    }
+    
+    func toggleLocationsList(){
+        withAnimation(.easeInOut) {
+            showLocationsList = !showLocationsList
+        }
+    }
+    
+    func showNextLocation(location: Location){
+        withAnimation(.easeInOut){
+            mapLocation = location
+            showLocationsList = false
+        }
+    }
+    
+    func nextButtonPressed() {
+        
+        // Get the current index
+        guard let currentIndex = locations.firstIndex(where: {$0 == mapLocation}) else{
+            print("Could not find current index")
+            return
+        }
+        
+        // Check if the nextIndex is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else{
+            // Next index is not valid
+            // restart from zero
+            
+            guard let firstLocation = locations.first else {return}
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        // Next index is valid
+        let nextLoacation = locations[nextIndex]
+        showNextLocation(location: nextLoacation)
+    }
+}
